6710b822d77f9541512ee8ae77f0c8914e90f94c
axelofan <cabanov.alexey@yandex.ru>
2023-09-13 18:41:52 +0400

    Изменил адрес приложения

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath:'/kege13generator/'
-})
+	transpileDependencies: true,
+	publicPath: '/oge9generator/',
+});
